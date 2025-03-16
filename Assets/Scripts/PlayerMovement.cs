faf389105c438212c457c2daba8f1d95b8b8d285
Khalil-BHL <khalil.bahlaouane@gmail.com>
2025-03-16 02:54:33 +0000

    base

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int facingDirection = 1;
+    public float speed = 5;
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    private bool isKnockedBack;
+
+    //Fixed Update is called 50x per frame
+    void FixedUpdate()
+    {
+        if(isKnockedBack == false) { 
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if (horizontal > 0 && transform.localScale.x < 0 ||
+            horizontal < 0 && transform.localScale.x > 0)
+        {
+            Flip();
+        }
+
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        rb.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+}
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+
+    public void Knockback(Transform enemy, float force, float stunTime)
+    {
+        isKnockedBack = true;
+        Vector2 direction = (transform.position - enemy.position).normalized;
+        rb.velocity = direction * force;
+        StartCoroutine(KnockbackCounter(stunTime));
+    }
+
+    IEnumerator KnockbackCounter(float stunTime)
+    {
+        yield return new WaitForSeconds(1);
+        rb.velocity = Vector2.zero;
+        isKnockedBack = false;
+    }
+}
